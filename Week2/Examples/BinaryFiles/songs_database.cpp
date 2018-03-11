265bd0f31b66216b2b0f1463a53282d3524db5c7
TsHristov <tsvetanhristov@yahoo.com>
2018-03-11 23:12:52 +0200

    Add SearchSongByName function

--- Week2/Examples/BinaryFiles/songs_database.cpp
+++ Week2/Examples/BinaryFiles/songs_database.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <string.h>
 using namespace std;
+
+enum RETURN_CODES {
+  SUCCESS,
+  ERROR_OPENING_FILE,
+  ERROR_WRITING_FILE,
+  ERROR_READING_FILE,
+};
 
 struct Song {
   char name[50];
-  unsigned length; // in milliseconds
+  unsigned length;
 };
 
 // Описание:
-//     Записва един обект тип Employee във файл с име `file_name`.
-bool WriteSongToFile(Song& song, const char* file_name) {
-  ofstream file(file_name, ios::binary);
+//     Записва един обект тип Song във файл с име `file_name`.
+int WriteSongToFile(Song& song, const char* file_name) {
+  ofstream file(file_name, ios::binary | ios::app);
 
   if(file.is_open()) {
     // Записваме обекта song във двоичния файл:
@@ -20,48 +27,73 @@ bool WriteSongToFile(Song& song, const char* file_name) {
     // Проверяваме дали писането е било успешно:
     if(file.bad()) {
       cerr << "Writing to " << file_name << " failed\n";
-      return false;
+      return ERROR_WRITING_FILE;
     }
 
     file.close();
-    return true;
+    return SUCCESS;
   }
 
-  return false;
+  return ERROR_WRITING_FILE;
 }
 
 // Описание:
 //     Прочита n-тата песен записана във файла:
-bool ReadSongFromFile(Song& song, const char* file_name, int n) {
+int ReadSongFromFile(Song& song, const char* file_name, int n) {
   ifstream file(file_name, ios::binary);
-
+  
   if(file.is_open()) {
     // Позиционираме маркера за четене от файла, като
-    // прескачаме n-1 Employee структури спрямо началото на файла:
+    // прескачаме n-1 Song структури спрямо началото на файла:
     file.seekg((n-1)*sizeof(Song), ios::beg);
     file.read((char*)&song, sizeof(Song));
 
-    // Проверяваме дали успешно сме прочели служител от файла:
+    // Проверяваме дали успешно сме прочели песен от файла:
     if(file.bad()){
       cerr << "Reading from " << file_name << " failed\n";
-      return false;
+      return ERROR_READING_FILE;
     }
 
     file.close();
-    return true;
+    return SUCCESS;
   }
   
-  return false;
+  return ERROR_READING_FILE;
+}
+
+// Описание:
+//   Търси песен по нейното име в файла и ако съществува връща информация за нея:
+int SearchSongByName(Song& song, const char* file_name, char* song_name) {
+  ifstream file(file_name, ios::binary);
+
+  if(file.is_open()) {
+    while(!file.eof()) {
+      file.read((char*)&song, sizeof(Song));
+      
+      if(file.bad()) {
+	cerr << "Read failed from " << file_name << "\n";
+	file.close();
+	return ERROR_READING_FILE;
+      }
+      
+      // Ако сме намерили песента с това име:
+      if(!strcmp(song.name, song_name)) {
+	file.close();
+	return SUCCESS;
+      }
+    }
+  }
+  return ERROR_READING_FILE;
 }
 
 int main() {  
   const char *file_name = "songs.bin";
 
   int songs_count;
-  cout << "Enter number of songs to be stored:";
+  cout << "Enter number of songs to be stored: ";
   cin >> songs_count;
-
-  // Въвеждаме песните една по една от стандартния вход (конзолата):
+  
+  // Въвеждаме песните една по една от стандартния вход:
   for(int i = 0; i < songs_count; i++) {
     Song song;
     cin.ignore();
@@ -71,15 +103,17 @@ int main() {
   }
 
   Song song;
-  int nth_song;
-  cout << "Enter a number from 1 - " << songs_count
-       << " to display information for the song:";
-  cin >> nth_song;
-  ReadSongFromFile(song, file_name, nth_song);
 
-  cout << "Song No: " << nth_song << " info:\n";
-  cout << song.name   << "\n";
-  cout << song.length << "\n";
+  char name[50];
+  cin.ignore();
+  cin.getline(name, 50);
+  
+  if(SearchSongByName(song, file_name, name) == SUCCESS) {
+    cout << "Song name: "   << song.name;
+    cout << "Song length: " << song.length;
+  } else {
+    cout << "Song " << name << " not found in the database :( \n";
+  }
   
   return 0;
 }
